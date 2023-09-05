c3c213da580439eb45a4a04438a0a9df941188f3
lucaszimmerman <lucaszimmermants@gmail.com>
2023-09-05 17:03:50 -0300

    manipulando a requisição

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,15 @@
 
-const offset = 0;
-const limit = 10;
-const url = 'https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}';
+const offset = 0
+const limit = 10
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
 //Quando finalizar a requisição, ele irá retornar está função com a response
-fetch(url).then(function (response) {
-    console.log(response)
-});
+fetch(url)
+    .then((response) => response.json()) //Aqui ele recebe o response e transforma em json
+    .then((jsonBody) =>console.log(jsonBody)) //Aqui ele irá mostrar o json
+    .catch((error) => console.error(error)) //Sempre que ocorrer um erro na requisição, o console mostrará uma mensagem de erro
+    // .finally(() => console.log('Requisição concluída'))//Sempre que a requisição finalizar, o console mostrará uma mensagem positiva de que a requisição foi concluída
+
+
+
 
