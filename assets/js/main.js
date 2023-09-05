f25e872b4a7e28778ac8e1736e20dd9a688b3dda
lucaszimmerman <lucaszimmermants@gmail.com>
2023-09-05 16:11:16 -0300

    primeira requisição

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,10 @@
-console.log('Sucesso')
+
+const offset = 0;
+const limit = 10;
+const url = 'https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}';
+
+//Quando finalizar a requisição, ele irá retornar está função com a response
+fetch(url).then(function (response) {
+    console.log(response)
+});
+
