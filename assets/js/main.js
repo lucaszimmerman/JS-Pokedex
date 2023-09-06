5a55ce4da545134826dde024175b9e62b000a22b
lucaszimmerman <lucaszimmermants@gmail.com>
2023-09-05 23:44:46 -0300

    cores para tipos

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,15 @@
-function convertPokemonTypesToLi(pokemonTypes) {
-    return pokemonTypes.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>`)
-}
 
 function convertPokemonToLi(pokemon) {
-    return ` <li class="pokemon">
-    <span class="number">#0${pokemon.order}</span> 
+    return ` 
+    <li class="pokemon ${pokemon.type}">
+    <span class="number">#0${pokemon.number}</span> 
     <span class="name">${pokemon.name}</span> 
     <div class="detail">
        <ol class="types">
-           ${convertPokemonTypesToLi(pokemon.types).join('')}
+           ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
        </ol>
-       <img src="${pokemon.sprites.other.dream_world.front_default}" alt="${pokemon.name}">
+       <img src="${pokemon.photo}" 
+       alt="${pokemon.name}">
    </div>
 </li>
     `
@@ -18,7 +17,7 @@ function convertPokemonToLi(pokemon) {
 
 const pokemonList = document.getElementById('pokemonList')
 
-   pokeApi.getPokemons().then((pokemons = []) => { //Aqui será feita a conversão do HTML para o pokemon específico
+pokeApi.getPokemons().then((pokemons = []) => { //Aqui será feita a conversão do HTML para o pokemon específico
     const newHtml = pokemons.map(convertPokemonToLi).join('') //.join junta todos os elementos do array
     pokemonList.innerHTML = newHtml
     })
